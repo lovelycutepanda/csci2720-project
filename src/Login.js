9e97f1a915c744a453be3b7c0dec674166802205
lovelycutepanda <kudicky@yahoo.com.hk>
2022-11-28 01:51:01 +0800

    connect react app with express server

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 
 class Login extends React.Component {
+
+  async getData() {
+    await fetch(process.env.REACT_APP_BASE_URL, {
+        method: "GET",
+        headers: new Headers({
+            "Content-Type": 'application/json',
+        })
+    })
+        .then((res) => res.json())
+        .then((obj) => {
+          console.log(obj.login, obj.password);
+        });
+}
+
   render() {
     return (
-      <p>Log in page</p>
+      <>
+        <p>Log in page</p>
+        <button onClick={() => {this.getData();}}>Get data</button>
+      </>
     );
   }
 }
---
+++ src/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+
+mongoose.connect('mongodb+srv://stu058:p308367W@cluster0.wenbhsm.mongodb.net/stu058');
+
+const db = mongoose.connection;
+const Schema = mongoose.Schema;
+
+/*
+const EventSchema = Schema({
+    eventId: { type: Number, required: true, unique: true },
+    name: { type: String, required: true },
+    loc: { type: Schema.Types.ObjectId, ref: 'Location' },
+    quota: { type: Number }
+});
+const Event = mongoose.model('Event', EventSchema);
+
+const LocationSchema = Schema({
+    locId: { type: Number, required: true, unique: true },
+    name: { type: String, required: true },
+    quota: { type: Number }
+});
+const Location = mongoose.model('Location', LocationSchema);
+*/
+
+// Upon connection failure
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// Upon successful connection
+db.once('open', () => {
+    console.log("Connection is open...");
+})
+    
+// handle ALL requests
+app.all('/*', (req, res) => {
+    // send this to client
+    let dataObject = {
+        login: "panda",
+        password: "1234"
+    }
+    res.json(dataObject);
+});
+
+
+// listen to port 4000
+const server = app.listen(4000);
