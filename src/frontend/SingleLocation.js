c17564ef45e67f94532fa18082defc5ac6806c75
marcotam2002 <marcotam2002@gmail.com>
2022-12-08 22:59:52 +0800

    added write comment function (upload and load comment not done)

--- src/frontend/SingleLocation.js
+++ src/frontend/SingleLocation.js
@@ -12,6 +12,8 @@ const SingleLocation = () => {
 
   const [location, setLocation] = useState({});
 
+  const [comment, setComment] = useState({});
+
   const back = () => {
     navigate('/user');
   }
@@ -25,8 +27,30 @@ const SingleLocation = () => {
     setLocation(location);
   }, [locationList])
 
+  // add comment function
+
+  var username = sessionStorage.getItem("user");
+
+  // get input comment
+  const getEvaluate = (e) => {
+    console.log('input: ' + e.target.value);
+    setComment(e.target.value);
+
+  }
+
+  // send comment to database and clear the textarea
+  const sendSubmit = () => {
+    let newComment = {
+      user: username,
+      comment: comment
+    };
+    console.log(newComment);
+    
+  }
+
+
   return (
-    <>
+    <div>
         Location ID: {locationId}
         <button onClick={() => back()}>Return to all locations</button>
 
@@ -55,7 +79,24 @@ const SingleLocation = () => {
             })}
           </tbody>
         </table>
-    </>
+        
+        <div>
+          {/* show all comments in history */}
+          <div className='comment'>
+            
+          </div>
+
+          {/* input new comment  */}
+          <h2>Leave your comment below: </h2>
+          <br/>
+          <form>
+            <textarea className='submit-comment' placeholder='Enter your comment' id='commentContent'
+              onChange={(e) => getEvaluate(e)}></textarea>
+          </form>
+          <button onClick={() => sendSubmit()}>Submit</button>
+        </div>
+
+    </div>
     
   );
 }
